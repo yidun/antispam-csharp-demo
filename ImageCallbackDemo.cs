5845f96f495a5bd2b7bec0f56e33367c9bec58c1
易盾开发 <dev@dun.163.com>
2016-07-18 19:55:05 +0800

    init

---
+++ ImageCallbackDemo.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Com.Netease.Is.Antispam.Demo
+{
+    class ImageCallbackDemo
+    {
+        public static void imageCallBack()
+        {
+            /** 产品密钥ID，产品标识 */
+            String secretId = "your_secret_id";
+            /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
+            String secretKey = "your_secret_key";
+            /** 业务ID，易盾根据产品业务特点分配 */
+            String businessId = "your_business_id";
+            /** 易盾反垃圾云服务图片离线检测结果获取接口地址 */
+            String apiUrl = "https://api.aq.163.com/v2/image/callback/results";
+            Dictionary<String, String> parameters = new Dictionary<String, String>();
+
+            long curr = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+            String time = curr.ToString();
+
+            // 1.设置公共参数
+            parameters.Add("secretId", secretId);
+            parameters.Add("businessId", businessId);
+            parameters.Add("version", "v2");
+            parameters.Add("timestamp", time);
+            parameters.Add("nonce", new Random().Next().ToString());
+
+            // 2.生成签名信息
+            String signature = Utils.genSignature(secretKey, parameters);
+            parameters.Add("signature", signature);
+
+            // 3.发送HTTP请求
+            HttpClient client = Utils.makeHttpClient();
+            String result = Utils.doPost(client, apiUrl, parameters, 10000);
+            if(result != null)
+            {
+                JObject ret = JObject.Parse(result);
+                int code = ret.GetValue("code").ToObject<Int32>();
+                String msg = ret.GetValue("msg").ToObject<String>();
+                if (code == 200)
+                {
+                    JArray array = (JArray)ret.SelectToken("result");
+                    foreach (var item in array)
+                    {
+                        JObject tmp = (JObject)item;
+                        String name = tmp.GetValue("name").ToObject<String>();
+                        JArray labels = (JArray)tmp.SelectToken("labels");
+                        foreach (var lable in labels)
+                        {
+                            JObject lableData = (JObject)lable;
+                            int label = lableData.GetValue("label").ToObject<Int32>();
+                            int level = lableData.GetValue("level").ToObject<Int32>();
+                            double rate = lableData.GetValue("rate").ToObject<Double>();
+                            Console.WriteLine(String.Format("label:{0}, level={1}, rate={2}", label, level, rate));
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("ERROR: code={0}, msg={1}", code, msg));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Request failed!");
+            }
+
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Netease.Is.Antispam.Demo
+{
+    class Utils
+    {
+        // 根据secretKey和parameters生成签名
+        public static String genSignature(String secretKey, Dictionary<String, String> parameters)
+        {
+            parameters = parameters.OrderBy(o => o.Key).ToDictionary(o => o.Key, p => p.Value);
+
+            StringBuilder builder = new StringBuilder();
+            foreach (KeyValuePair<String, String> kv in parameters)
+            {
+                builder.Append(kv.Key).Append(kv.Value);
+            }
+            builder.Append(secretKey);
+            String tmp = builder.ToString();
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(Encoding.UTF8.GetBytes(tmp));
+            builder.Clear();
+            foreach (byte b in result)
+            {
+                builder.Append(b.ToString("x2").ToLower());
+            }
+            return builder.ToString();
+        }
+
+        public static HttpClient makeHttpClient()
+        {
+            HttpClient client = new HttpClient() {};
+            client.DefaultRequestHeaders.Connection.Add("keep-alive");
+            client.SendAsync(new HttpRequestMessage
+            {
+                Method = new HttpMethod("HEAD"),
+                RequestUri = new Uri("https://api.aq.163.com")
+            }).Wait();
+            return client;
+        }
+
+        // 执行post操作
+        public static String doPost(HttpClient client, String url, Dictionary<String, String> parameters, int timeOutInMillisecond)
+        {
+            HttpContent content = new FormUrlEncodedContent(parameters);
+            Task<HttpResponseMessage> task = client.PostAsync(url, content);
+            if (task.Wait(timeOutInMillisecond))
+            {
+                HttpResponseMessage response = task.Result;
+                if(response.StatusCode == HttpStatusCode.OK)
+                {
+                    Task<string> result = response.Content.ReadAsStringAsync();
+                    result.Wait();
+                    return result.Result;
+                }
+            }
+            return null;
+        }
+    }
+}
