988f9e3606f4bac0bc76be7f6a62afff91d8c980
雷绍波 <leishaobo_cafe@163.com>
2016-12-29 16:37:36 +0800

    修改文本，图片在线检测和回调接口，新增文本和图片查询接口

--- TextQueryByTaskIdsDemo.cs
+++ TextQueryByTaskIdsDemo.cs
@@ -1,13 +1,17 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Net.Http;
 
 namespace Com.Netease.Is.Antispam.Demo
 {
-    class ImageCallbackDemo
+    class TextQueryByTaskIdsDemo
     {
-        public static void imageCallBack()
+
+        public static void textQueryByTaskIds()
         {
             /** 产品密钥ID，产品标识 */
             String secretId = "your_secret_id";
@@ -15,8 +19,8 @@ namespace Com.Netease.Is.Antispam.Demo
             String secretKey = "your_secret_key";
             /** 业务ID，易盾根据产品业务特点分配 */
             String businessId = "your_business_id";
-            /** 易盾反垃圾云服务图片离线检测结果获取接口地址 */
-            String apiUrl = "https://api.aq.163.com/v3/image/callback/results";
+            /** 易盾反垃圾云服务文本结果查询接口地址 */
+            String apiUrl = "https://api.aq.163.com/v1/text/query/task";
             Dictionary<String, String> parameters = new Dictionary<String, String>();
 
             long curr = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
@@ -25,18 +29,25 @@ namespace Com.Netease.Is.Antispam.Demo
             // 1.设置公共参数
             parameters.Add("secretId", secretId);
             parameters.Add("businessId", businessId);
-            parameters.Add("version", "v3");
+            parameters.Add("version", "v1");
             parameters.Add("timestamp", time);
             parameters.Add("nonce", new Random().Next().ToString());
 
-            // 2.生成签名信息
+            // 2.设置私有参数
+            ISet<String> taskIds=new HashSet<String>();
+            taskIds.Add("ecac3bc976674c36bfc5c06445243306");
+            taskIds.Add("9fb210fa19a343f69b7e287912fa1ba6");
+            parameters.Add("taskIds", JArray.FromObject(taskIds).ToString());
+           
+
+            // 3.生成签名信息
             String signature = Utils.genSignature(secretKey, parameters);
             parameters.Add("signature", signature);
 
-            // 3.发送HTTP请求
+            // 4.发送HTTP请求
             HttpClient client = Utils.makeHttpClient();
             String result = Utils.doPost(client, apiUrl, parameters, 10000);
-            if(result != null)
+            if (result != null)
             {
                 JObject ret = JObject.Parse(result);
                 int code = ret.GetValue("code").ToObject<Int32>();
@@ -44,40 +55,23 @@ namespace Com.Netease.Is.Antispam.Demo
                 if (code == 200)
                 {
                     JArray array = (JArray)ret.SelectToken("result");
-                    if (array == null || !array.HasValues)
-                    {
-                        Console.WriteLine("暂时没有人工复审结果需要获取，请稍后重试！");
-                    }
-                    
+                    Console.WriteLine(array);
+
                     foreach (var item in array)
                     {
                         JObject tmp = (JObject)item;
-                        String name = tmp.GetValue("name").ToObject<String>();
+                        int action = tmp.GetValue("action").ToObject<Int32>();
                         String taskId = tmp.GetValue("taskId").ToObject<String>();
-                        JArray labels = (JArray)tmp.SelectToken("labels");
-                        Console.WriteLine(String.Format("taskId={0}，name={1}，labels：", taskId, name));
-                        int maxLevel = -1;
-                        // 产品需根据自身需求，自行解析处理，本示例只是简单判断分类级别
-                        foreach (var lable in labels)
+                        int status = tmp.GetValue("status").ToObject<Int32>();
+                        String callback = tmp.GetValue("callback").ToObject<String>();
+                        JArray labelArray = (JArray)tmp.SelectToken("labels");
+                        if (action == 0)
                         {
-                            JObject lableData = (JObject)lable;
-                            int label = lableData.GetValue("label").ToObject<Int32>();
-                            int level = lableData.GetValue("level").ToObject<Int32>();
-                            double rate = lableData.GetValue("rate").ToObject<Double>();
-                            Console.WriteLine(String.Format("label:{0}, level={1}, rate={2}", label, level, rate));
-                            maxLevel = level > maxLevel ? level : maxLevel;
+                            Console.WriteLine(String.Format("taskId={0}，status={1},callback={2}，文本查询结果：通过", taskId, status, callback));
                         }
-
-                        switch (maxLevel)
+                        else if (action == 2)
                         {
-                            case 0:
-                                Console.WriteLine("#图片人工复审结果：最高等级为\"正常\"\n");
-                                break;
-                            case 2:
-                                Console.WriteLine("#图片人工复审结果：最高等级为\"确定\"\n");
-                                break;
-                            default:
-                                break;
+                            Console.WriteLine(String.Format("taskId={0}，status={1},callback={2}，文本查询结果：不通过，分类信息如下：{3}", taskId, status, callback, labelArray));
                         }
                     }
                 }
@@ -90,7 +84,6 @@ namespace Com.Netease.Is.Antispam.Demo
             {
                 Console.WriteLine("Request failed!");
             }
-
         }
     }
 }
