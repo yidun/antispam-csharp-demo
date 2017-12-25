426d2a60cdbb1759f06a0727f935dbfc7007b5d7
周泽华 <zhouzehua@corp.netease.com>
2017-12-25 09:21:38 +0000

    change domain of API endpoint

--- ImageQueryByTaskIdsDemo.cs
+++ ImageQueryByTaskIdsDemo.cs
@@ -16,7 +16,7 @@ namespace Com.Netease.Is.Antispam.Demo
             /** 业务ID，易盾根据产品业务特点分配 */
             String businessId = "your_business_id";
             /** 易盾反垃圾云服务图片离线检测结果获取接口地址 */
-            String apiUrl = "https://api.aq.163.com/v1/image/query/task";
+            String apiUrl = "https://as.dun.163yun.com/v1/image/query/task";
             Dictionary<String, String> parameters = new Dictionary<String, String>();
 
             long curr = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
