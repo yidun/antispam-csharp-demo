93cd0702473616a37ec92ce355a9d7ea1b012d28
maxiaofeng <maxiaofeng@corp.netease.com>
2019-06-10 20:11:08 +0800

    #注释非必要参数，提交新接口demo

--- FileSolutionSubmitApiDemo.cs
+++ FileSolutionSubmitApiDemo.cs
@@ -1,22 +1,20 @@
-﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 
 namespace Com.Netease.Is.Antispam.Demo
 {
-    class TextCheckApiDemo
+    class FileSolutionSubmitApiDemo
     {
-        public static void textCheck()
-        {
+        public static void fileSubmit()
+        {     
             /** 产品密钥ID，产品标识 */
             String secretId = "your_secret_id";
             /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
             String secretKey = "your_secret_key";
-            /** 业务ID，易盾根据产品业务特点分配 */
-            String businessId = "your_business_id";
-            /** 易盾反垃圾云服务文本在线检测接口地址 */
-            String apiUrl = "https://as.dun.163yun.com/v3/text/check";
+            /** 易盾反垃圾云服务文档解决方案信息提交接口地址  */
+            String apiUrl = "https://as-file.dun.163yun.com/v1/file/submit";
             Dictionary<String, String> parameters = new Dictionary<String, String>();
 
             long curr = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
@@ -24,21 +22,18 @@ namespace Com.Netease.Is.Antispam.Demo
 
             // 1.设置公共参数
             parameters.Add("secretId", secretId);
-            parameters.Add("businessId", businessId);
-            parameters.Add("version", "v3.1");
+            parameters.Add("version", "v1");
             parameters.Add("timestamp", time);
             parameters.Add("nonce", new Random().Next().ToString());
 
             // 2.设置私有参数
             parameters.Add("dataId", "ebfcad1c-dba1-490c-b4de-e784c2691768");
-            parameters.Add("content", "易盾测试内容");
-			// parameters.Add("dataType", "1");
+            parameters.Add("url", "http://xxx.xxx.com/xxxx");
+            // parameters.Add("dataType", "1");
+            // parameters.Add("checkFlag", "3");
             // parameters.Add("ip", "123.115.77.137");
-            // parameters.Add("account", "csharp@163.com");
-            // parameters.Add("deviceType", "4");
-            // parameters.Add("deviceId", "92B1E5AA-4C3D-4565-A8C2-86E297055088");
+            // parameters.Add("account", "java@163.com");
             // parameters.Add("callback", "ebfcad1c-dba1-490c-b4de-e784c2691768");
-            // parameters.Add("publishTime", time);
 
             // 3.生成签名信息
             String signature = Utils.genSignature(secretKey, parameters);
@@ -47,7 +42,6 @@ namespace Com.Netease.Is.Antispam.Demo
             // 4.发送HTTP请求
             HttpClient client = Utils.makeHttpClient();
             String result = Utils.doPost(client, apiUrl, parameters, 1000);
-            Console.WriteLine(result);
             if(result != null)
             {
                 JObject ret = JObject.Parse(result);
@@ -57,20 +51,7 @@ namespace Com.Netease.Is.Antispam.Demo
                 {
                     JObject resultObject = (JObject)ret["result"];
                     String taskId = resultObject["taskId"].ToObject<String>();
-                    int action = resultObject["action"].ToObject<Int32>();
-                    JArray labelArray = (JArray)resultObject.SelectToken("labels");
-                    if (action == 0)
-                    {
-                        Console.WriteLine(String.Format("taskId={0}，文本机器检测结果：通过", taskId));
-                    }
-                    else if (action == 1)
-                    {
-                        Console.WriteLine(String.Format("taskId={0}，文本机器检测结果：嫌疑，需人工复审，分类信息如下：{1}", taskId, labelArray));
-                    }
-                    else if (action == 2)
-                    {
-                        Console.WriteLine(String.Format("taskId={0}，文本机器检测结果：不通过，分类信息如下：{1}", taskId, labelArray));
-                    }
+                    String dataId = resultObject["dataId"].ToObject<String>();
                 }
                 else
                 {
