a1690b4e016ec666df6ea97983764da615aab636
hzhumin1 <hzhumin1@corp.netease.com>
2020-05-11 13:13:21 +0800

    #YDNR-9515【网站检测解决方案demo】c#版本demo补充

---
+++ CrawlerSolutionCallbackApiDemo.cs
@@ -0,0 +1,67 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Com.Netease.Is.Antispam.Demo
+{
+    class CrawlerSolutionCallbackApiDemo
+    {
+        public static void crawlerSolutionCallBack()
+        {
+            /** 产品密钥ID，产品标识 */
+            String secretId = "your_secret_id";
+            /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
+            String secretKey = "your_secret_key";
+            /** 易盾反垃圾云服务网站检测解决方案提交接口地址 */
+            String apiUrl = "http://as.dun.163yun.com/v1/crawler/callback/results";
+            Dictionary<String, String> parameters = new Dictionary<String, String>();
+
+            long curr = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+            String time = curr.ToString();
+
+            // 1.设置公共参数
+            parameters.Add("secretId", secretId);
+            parameters.Add("version", "v1.0");
+            parameters.Add("timestamp", time);
+            parameters.Add("nonce", new Random().Next().ToString());
+
+            // 2.生成签名信息
+            String signature = Utils.genSignature(secretKey, parameters);
+            parameters.Add("signature", signature);
+
+            // 3.发送HTTP请求
+            HttpClient client = Utils.makeHttpClient();
+            String resultResponse = Utils.doPost(client, apiUrl, parameters, 10000);
+            if(resultResponse != null)
+            {
+                JObject ret = JObject.Parse(resultResponse);
+                int code = ret.GetValue("code").ToObject<Int32>();
+                String msg = ret.GetValue("msg").ToObject<String>();
+                if (code == 200)
+                {
+                    JArray array = (JArray)ret.SelectToken("result");
+                    foreach (var item in array)
+                    {
+                        JObject tmp = (JObject)item;
+                        String callback = tmp.GetValue("callback").ToObject<String>();
+                        String taskId = tmp.GetValue("taskId").ToObject<String>();
+                        String dataId = tmp.GetValue("dataId").ToObject<String>();
+                        // 证据信息
+                        JObject evidences = (JObject)tmp.SelectToken("evidences");
+                        int result = tmp.GetValue("result").ToObject<Int32>();
+                        Console.WriteLine(String.Format("网站检测解决方案结果：taskId={0}：dataId={1}：result={2}：callback={3}", taskId, dataId, result, callback));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("ERROR: code={0}, msg={1}", code, msg));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Request failed!");
+            }
+        }
+    }
+}
